ca38da3d8a474bbced06c576746909a5ddd0b8c0
hariprasathm-tringapps <94889177+hariprasathm-tringapps@users.noreply.github.com>
2021-12-01 14:45:48 +0530

    Update script5.js

--- script5.js
+++ script5.js
@@ -23,15 +23,15 @@ function readFormData() {
 function insertNewRecord(data) {
     var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
+    var cell1 = newRow.insertCell(0);
     cell1.innerHTML = data.fullName;
-    cell2 = newRow.insertCell(1);
+    var cell2 = newRow.insertCell(1);
     cell2.innerHTML = data.empCode;
-    cell3 = newRow.insertCell(2);
+    var cell3 = newRow.insertCell(2);
     cell3.innerHTML = data.salary;
-    cell4 = newRow.insertCell(3);
+    var cell4 = newRow.insertCell(3);
     cell4.innerHTML = data.city;
-    cell4 = newRow.insertCell(4);
+    var cell4 = newRow.insertCell(4);
     cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button>
                        <button onClick="onDelete(this)">Delete</button>`;
 }
